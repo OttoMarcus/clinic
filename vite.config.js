544fc00234e482b827ca82ca13feac6cf5ca2947
OttoMarcus <invictum@zoho.com>
2024-10-06 23:13:30 +0300

    perf(package.json): dev <- start command

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 // import vitePluginSass from 'vite-plugin-sass';
 // import vitePluginCssModules from 'vite-plugin-css-modules';
 
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     svgr(),
+//     // vitePluginSass(),
+//     // vitePluginCssModules(),
+//   ]
+// });
+
+export default {
   plugins: [
-    react(),
-    svgr(),
-    // vitePluginSass(),
-    // vitePluginCssModules(),
-  ]
-});
+      react(),
+      svgr(),
+  ],
+};
