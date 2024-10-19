38bb39953543dd0f8f4fdbbae0279d13792263d7
OttoMarcus <invictum@zoho.com>
2024-10-19 23:06:41 +0300

    removed pre-push git-hook husky

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import svgr from 'vite-plugin-svgr';
+//
+// export default defineConfig({
+//     plugins: [react(), svgr()],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react()],
 });
